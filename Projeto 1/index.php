61ee6be901aa459c6132e1df0d8832995d0268d7
Faell <rhafahenrique@hotmail.com>
2024-02-09 16:20:40 -0300

    BackEnd - 0.1 - Adição dos pacientes na tabela

--- Projeto 1/index.php
+++ Projeto 1/index.php
@@ -1,5 +1,84 @@
 <?php
 
+echo '
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>Pacientes</title>
+    <link rel="stylesheet" href="index.css">
+    <script defer src="index.js"></script>
+</head>
+<body>
+    
+    <header id="campo-cabecalho">
+        <div id="campo-botao-cabecalho">
+            <button class="botao-cabecalho">Solicitações Clínicas</button>
+            <button class="botao-cabecalho">Listagem de Solicitações</button>
+        </div>
+    </header>
 
+    <article id="campo-corpo">
+        <div id="elemento-pesquisa">
+            <img src="icones/search.png">
+            <input id="input-pesquisa" placeholder="Pesquisar" type="text">
+        </div>
+
+        <section id="sessao-consulta">
+            <table id="tabela-consulta">
+                <tr class="linha-tabela-cabecalho">
+                    <th class="coluna-cabecalho-tabela">Paciente</th>
+                    <th class="coluna-cabecalho-tabela">Nascimento</th>
+                    <th class="coluna-cabecalho-tabela">CPF</th>
+                    <th class="coluna-cabecalho-tabela">Ações</th>
+                </tr>
+';
+
+// Adicionando pacientes da tabela
+
+$autorizacao_execucao_script_database=true;
+require_once("database.php");
+
+$retornoConsultaDB=consultarPacientes();
+
+$quantidadePacientes=count($retornoConsultaDB["pacientes"]);
+for($contadorPacientes=0; $contadorPacientes<$quantidadePacientes; $contadorPacientes++){
+    $nomePacienteAtual=$retornoConsultaDB["pacientes"][$contadorPacientes]["nome"];
+    $dataNascPacienteAtual=$retornoConsultaDB["pacientes"][$contadorPacientes]["dataNasc"];
+    $cpfPacienteAtual=$retornoConsultaDB["pacientes"][$contadorPacientes]["cpf"];
+
+    echo'
+                <tr class="linha-tabela-conteudo">
+                    <td class="coluna-conteudo-tabela">'.$nomePacienteAtual.'</td>
+                    <td class="coluna-conteudo-tabela">'.$dataNascPacienteAtual.'</td>
+                    <td class="coluna-conteudo-tabela">'.$cpfPacienteAtual.'</td>
+                    <td class="coluna-conteudo-tabela-botao"><button class="botao-prosseguir-tabela">Proseguir</button></td>
+                </tr>';
+}
+
+echo'
+            </table>
+        </section>
+        <section id="sessao-paginador">
+            <div id="campo-paginador">
+                <ul id="elementos-paginador">
+                    <img id="seta-anterior-pagina" src="icones/seta.png">';
+
+echo'
+                    <li id="elemento-pagina-atual">1</li>
+                    <li class="elemento-pagina">2</li>';
+
+echo'
+                    <img id="seta-proxima-pagina" src="icones/seta.png">
+                </ul>
+            </div>
+        </section>
+    </article>
+    <footer id="campo-rodape">
+        <p id="texto-rodape">Rhafaell_SITCON</p>
+    </footer>
+
+</body>
+</html>';
 
 ?>
